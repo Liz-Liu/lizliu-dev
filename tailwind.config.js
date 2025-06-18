57aed2fce0a4673e2b5ef03bb048348a864aebee
Liz Liu <liangliang0628@gmail.com>
2025-06-18 16:56:08 +0800

    chore: setup project structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',  // 用 class 切換 dark mode
   content: [
-    "./src/**/*.{html,ts}"
+    "./src/**/*.{html,ts}",
   ],
   plugins: [],
 };
