71a25389a5ac22e1863870292c1069218f1ef505
Liz Liu <liangliang0628@gmail.com>
2025-06-19 13:22:00 +0800

    feat: add section component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AboutComponent } from './sections/about.component';
+import { WorkExperienceComponent } from './sections/work-experience.component';
+import { SkillsComponent } from './sections/skills.component';
+import { ProjectsComponent } from './sections/projects.component';
+import { ContributionsComponent } from './sections/contributions.component';
 
 @Component({
   standalone: true,
   selector: 'app-home',
-  imports: [],
+  imports: [ CommonModule,
+  AboutComponent,
+  WorkExperienceComponent,
+  SkillsComponent,
+  ProjectsComponent,
+  ContributionsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
